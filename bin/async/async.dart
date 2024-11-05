a5fa959a5fea3b21651e0dc5d02fffb3ea908af1
Mohamed Sayed <mohammedsayed7414@gmail.com>
2024-11-05 04:23:22 +0200

    add async.dart

---
+++ bin/async/async.dart
@@ -0,0 +1,44 @@
+
+void main()
+{
+/*
+session content:
+  1. Exceptions                  => try & catch
+  2. Asynchronous programming    => Future, async, await
+  3. splash screen
+*/
+
+
+  // List<int> l=[1,2,3];
+  // print(l[6]);
+
+  // int a=0,b=10;
+  // int res = b ~/ a;
+  // print(res);
+
+
+  // try{
+  //   int x=int.parse(stdin.readLineSync()!);
+  //   int res = 20 ~/ x;
+  //   print(res);
+  // }on RangeError catch(e){
+  //   print('range error');
+  // } catch(e){
+  //   print('sorry!');
+  //   print(e.toString());
+  // }finally{
+  //
+  // }
+  //
+  // print('done');
+
+
+  // print('1');
+  // Future.delayed(Duration(seconds: 4),(){
+  //   print('2');
+  // });
+  // print('3');
+  // print('4');
+
+}
+
